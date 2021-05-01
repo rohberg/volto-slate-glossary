2200ac4fad84f9b2264dcdb6c9d8f27b8c5a581e
Katja Süss <k.suess@rohberg.ch>
2021-05-01 23:09:53 +0200

    fetch glossaryterms

--- src/components/Tooltips.jsx
+++ src/components/Tooltips.jsx
@@ -1,6 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { searchContent } from '@plone/volto/actions';
 
 const Tooltips = () => {
+  const dispatch = useDispatch();
+  const glossaryterms = useSelector(
+    (state) => state.search.subrequests.glossaryterms?.items,
+  );
+  console.log('glossaryterms', glossaryterms);
+
+  useEffect(() => {
+    const portalTypes = ['Term'];
+    const workflows = ['published'];
+    dispatch(
+      searchContent(
+        '/',
+        {
+          portal_type: portalTypes,
+          review_state: workflows,
+          sort_on: 'title',
+        },
+        'glossaryterms',
+      ),
+    );
+  }, [dispatch]);
+
   return __CLIENT__ && <div>I am Tooltips component</div>;
 };
 
