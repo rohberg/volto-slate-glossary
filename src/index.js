b1e034ec5d408207bddc0eefb71f323336cd9d5c
Katja Süss <k.suess@rohberg.ch>
2021-05-02 18:08:39 +0200

    SSR

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import { serializeNodes } from './utils';
-const applyConfig = (config) => {
+import { searchContent } from '@plone/volto/actions';
+
+const searchoptions = [
+  '/',
+  {
+    portal_type: ['Term'],
+    review_state: ['published'],
+    sort_on: 'id',
+    sort_order: 'descending',
+  },
+  'glossaryterms',
+];
+
+export default (config) => {
+  config.settings.asyncPropsExtenders = [
+    ...(config.settings.asyncPropsExtenders ?? []),
+    {
+      path: '/',
+      extend: (dispatchActions) => {
+        if (
+          dispatchActions.filter(
+            (asyncAction) => asyncAction.key === 'glossaryterms',
+          ).length === 0
+        ) {
+          dispatchActions.push({
+            key: 'glossaryterms',
+            promise: ({ location, store: { dispatch } }) =>
+              __SERVER__ && dispatch(searchContent(searchoptions)),
+          });
+        }
+        return dispatchActions;
+      },
+    },
+  ];
+
   return config;
 };
-
-export default applyConfig;
-
-// TODO fetch glossary data on SSR
 
 export { serializeNodes };
