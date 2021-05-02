c37c57172c24be34153c230b9d9dc1658c906467
Katja Süss <k.suess@rohberg.ch>
2021-05-02 18:09:44 +0200

    Tooltips done: custom serializeNodes

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,80 @@
 // TODO serializeNodes
 import React from 'react';
+import { useSelector } from 'react-redux';
 import cx from 'classnames';
-import { isEmpty, isEqual, omit } from 'lodash';
+import { flatten, isEqual, reverse } from 'lodash';
+import { Button, Popup } from 'semantic-ui-react';
 import { Node, Text } from 'slate';
 
 import { Element, Leaf } from 'volto-slate/editor/render';
+
+const addGlossaryTooltips = (txt) => {
+  const glossaryterms = useSelector(
+    (state) => state.search.subrequests.glossaryterms?.items,
+  );
+  let result = [{ type: 'text', val: txt }];
+  if (glossaryterms !== undefined) {
+    glossaryterms.forEach((term) => {
+      result = result.map((chunk) => {
+        if (chunk.type === 'text') {
+          let splittedtext = chunk.val.split(term.title);
+          if (splittedtext.length > 1) {
+            chunk = [{ type: 'text', val: splittedtext[0] }];
+            splittedtext.splice(0, 1);
+            splittedtext.forEach((t) => {
+              chunk.push({
+                type: 'popup',
+                val: term.title,
+              });
+              chunk.push({ type: 'text', val: t });
+            });
+          }
+        }
+        return chunk;
+      });
+      result = flatten(result);
+    });
+  }
+  result = flatten(result);
+  return result;
+};
 
 const serializeData = (node) => {
   return JSON.stringify({ type: node.type, data: node.data });
 };
 
 export const serializeNodes = (nodes, getAttributes) => {
-  console.log('** serializeNodes (customized)');
   const editor = { children: nodes || [] };
+  const glossaryterms = useSelector(
+    (state) => state.search.subrequests.glossaryterms?.items,
+  );
 
   const _serializeNodes = (nodes) => {
     return (nodes || []).map(([node, path], i) => {
       return Text.isText(node) ? (
         <Leaf path={path} leaf={node} text={node} mode="view" key={path}>
-          <b>TODO wrap term with abbr: {node.text}</b>
+          {addGlossaryTooltips(node.text).map((el, j) => {
+            if (el.type === 'text') {
+              return <span key={j}>{el.val}</span>;
+            } else {
+              let idx = glossaryterms.findIndex((gt) => gt.title === el.val);
+              let descr = glossaryterms[idx]['description'];
+              return (
+                <Popup
+                  trigger={<span className="glossarytooltip">{el.val}</span>}
+                  key={j}
+                >
+                  <Popup.Content>
+                    <div>
+                      <b>{el.val}</b>
+                      <br />
+                      <span>{descr}</span>
+                    </div>
+                  </Popup.Content>
+                </Popup>
+              );
+            }
+          })}
         </Leaf>
       ) : (
         <Element
