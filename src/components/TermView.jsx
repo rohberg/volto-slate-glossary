691853d72e43fbf20e1d9b37409162b36673b5a4
ksuess <k.suess@rohberg.ch>
2022-03-07 16:48:28 +0100

    Update TermView.jsx

--- src/components/TermView.jsx
+++ src/components/TermView.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@plone/volto/components';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import backSVG from '@plone/volto/icons/back.svg';
 
 const TermView = ({ content }) => {
   return (
-    <Container className="view-wrapper">
-      <article id="content">
-        <header>
-          <h1 className="documentFirstHeading">{content.title}</h1>
-        </header>
-        <section id="content-core">
-          {content.variations &&
-            content.variations.map((el) => <div key={el}>{el}</div>)}
-          {content.definition && (
-            <p
-              dangerouslySetInnerHTML={{
-                __html: content.definition.data || content.description,
-              }}
-            />
-          )}
-        </section>
-      </article>
-    </Container>
+    <>
+      <Container className="view-wrapper">
+        <article id="content">
+          <header>
+            <h1 className="documentFirstHeading">{content.title}</h1>
+          </header>
+          <section id="content-core">
+            {content.variants && (
+              <dl className="variants">
+                {content.variants.map((el) => (
+                  <dt key={el}>{el}</dt>
+                ))}
+              </dl>
+            )}
+            {content.definition && (
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: content.definition?.data || [],
+                }}
+              />
+            )}
+            <Link to={flattenToAppURL(content.parent['@id'])} className="item">
+              <Icon
+                name={backSVG}
+                className="contents circled"
+                size="30px"
+                title="back"
+              />
+            </Link>
+          </section>
+        </article>
+      </Container>
+    </>
   );
 };
 
