ca6ab9f74763271612fdac84393d0c540524aa30
Katja Süss <k.suess@rohberg.ch>
2024-10-23 13:51:08 +0200

    Update policy package

--- packages/policy/src/index.js
+++ packages/policy/src/index.js
@@ -3,28 +3,15 @@ import { Tooltips } from '@rohberg/volto-slate-glossary/components';
 const applyConfig = (config) => {
   // glossary tooltips
   config.settings.glossary.caseSensitive = false;
-  config.settings.glossary.matchOnlyFirstOccurence = true;
+  config.settings.glossary.matchOnlyFirstOccurence = false;
 
-  // Tootips everywhere
+  // Tooltips everywhere
   config.settings.appExtras = [
     ...config.settings.appExtras,
     {
       match: '/',
       component: Tooltips,
     },
-  ];
-
-  // DEBUG Toltips only in /documentation and /hints
-  config.settings.appExtras = [
-    ...config.settings.appExtras,
-    {
-      match: '/documentation',
-      component: Tooltips,
-    },
-    // {
-    //   match: '/hints',
-    //   component: Tooltips,
-    // },
   ];
 
   return config;
